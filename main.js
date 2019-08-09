0cc5a34f8977c5d50f482885d76284ac840f246c
marcusmASC6 <21marcusm@lrei.org>
2019-08-09 16:16:09 -0400

    Header and Searc bar for Schools

---
+++ main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("SchoolName");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+document.getElementById("schoolForm").style.width= "40vw";
+document.getElementById("schoolForm").style.fontStyle="14pt";
