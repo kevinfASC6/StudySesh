20906949f90cd3dbe3aedadddb8389959e1e7a10
axelbASC6 <bolyaxel@gmail.com>
2019-08-12 11:20:55 -0400

    debugging login

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 var firebaseConfig = {
     apiKey: "AIzaSyDXqHdzV8IoYakz4tr5rhDHL4NAfXTh4ak",
     authDomain: "study-sesh-10dbf.firebaseapp.com",
@@ -36,7 +38,7 @@ var firebaseConfig = {
       }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'redirect',
+    signInFlow: 'popup',
     signInSuccessUrl: 'https://kevinfasc6.github.io/StudySesh/index',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
