3b2504f6521a03878d28db098581543a79444044
axelbASC6 <bolyaxel@gmail.com>
2019-08-14 09:36:38 -0400

    ACtualy fix login

--- login.js
+++ login.js
@@ -1,6 +1,3 @@
-
-console.log("somewhere");
-
 var firebaseConfig = {
     apiKey: "AIzaSyDXqHdzV8IoYakz4tr5rhDHL4NAfXTh4ak",
     authDomain: "study-sesh-10dbf.firebaseapp.com",
@@ -10,45 +7,10 @@ var firebaseConfig = {
     messagingSenderId: "527300603970",
     appId: "1:527300603970:web:d0cd1d4569892599"
   };
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
-  function manualLogin(){
-  
-    var email = "example@example.com";
-  var password = "testPasswprd";
-//create user email and pass
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
-
-  
-  //Sign In User with Email and Password
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-});
-
-    
-  
-  //sign out function
-  firebase.auth().signOut().then(function() {
-    console.log("Sign-out successful")
-  }).catch(function(error) {
-    console.log(error)
-  });
-}
-  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 var uiConfig = {
-  signInSuccessUrl: 'https://kevinfasc6.github.io/StudySesh/',
+  signInSuccessUrl: "index.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.EmailAuthProvider.PROVIDER_ID
@@ -56,14 +18,10 @@ var uiConfig = {
   // tosUrl and privacyPolicyUrl accept either url string or a callback
   // function.
   // Terms of service url/callback.
-  tosUrl: "https://google.com",
-  // Privacy policy url/callback.
-  privacyPolicyUrl: function() {
-    window.location.assign('lol.com');
-  }
+  tosUrl: "https://google.com"
 };
 
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
